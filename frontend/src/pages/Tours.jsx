3c78fa276eea6afdd8f8494f2b59433f44e9b6ef
Ayush Verma <ayush.verma281201@gmail.com>
2023-07-22 01:23:25 +0530

    first commit

---
+++ frontend/src/pages/Tours.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CommonSection from '../shared/CommonSection'
+
+import "../styles/tour.css";
+const Tours = () => {
+  return (
+    <>
+    <CommonSection title={"All Tours"}/>
+    </>
+  );
+};
+
+export default Tours
